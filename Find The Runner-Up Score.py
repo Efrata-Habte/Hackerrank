06ed592b23dce03ccc0ee5fe309bcb272a73af50
Efrata Habte <efratahabte1213@gmail.com>
2025-09-18 21:41:54 +0300

    Create Find The Runner-Up Score.py

---
+++ Find The Runner-Up Score.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    arr=list(arr)
+    max_num=max(arr)
+    
+    while max_num in arr:
+        arr.remove(max_num)
+        
+    print(max(arr))
